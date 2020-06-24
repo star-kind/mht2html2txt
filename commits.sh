dc8bc8a632cf80d2d364f3443cf4ecf2821e24ce
AllStarGH <duernna@163.com>
2020-06-25 02:43:32 +0800

    it solves the problem of disorderly code in reading and writing Chinese characters

--- commits.sh
+++ commits.sh
@@ -3,8 +3,7 @@
 #git config user.email "duernna@163.com"
 #git config user.name  "AllStarGH"
 
-#origin:远端仓库的名字
-#git remote add origin https://gitee.com/yuanjingpeng/mht2html2txt.git
+#origin:远方仓库的名字
 #git remote add origin https://github.com/AllStarGH/mht2html2txt.git
 
 git status
@@ -13,10 +12,12 @@ git add .
 
 git commit -m "it solves the problem of disorderly code in reading and writing Chinese characters"
 
-#首次提交报错:fatal: 拒绝合并无关的历史
-git pull origin master --allow-unrelated-histories
+#如果第一次提交报错:更新被拒绝,因为您当前分支的最新提交落后于其对应的远程分支
+#可以再试一次,也是下面这句话
+#git pull origin master --allow-unrelated-histories
 
 #master:当前分支名
-#git pull origin master
+#首次上传成功后,就切换为下面这句
+git pull origin master
 
 git push origin master  
